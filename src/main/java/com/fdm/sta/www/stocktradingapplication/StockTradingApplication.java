ac78748142b209568c3989bfcbb20e3c9d753de7
Hamza Gobir <hgobir@googlemail.com>
2021-02-07 13:35:51 +0000

    first commit

---
+++ src/main/java/com/fdm/sta/www/stocktradingapplication/StockTradingApplication.java
@@ -0,0 +1,13 @@
+package com.fdm.sta.www.stocktradingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTradingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockTradingApplication.class, args);
+    }
+
+}
